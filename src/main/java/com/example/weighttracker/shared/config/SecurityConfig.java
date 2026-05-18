09103b67d19b217bc03e7b77392921faaa9eb03f
bumbens <bumbens@icloud.com>
2026-05-18 14:26:50 +0200

    adjusted configuration for production deployment

--- src/main/java/com/example/weighttracker/shared/config/SecurityConfig.java
+++ src/main/java/com/example/weighttracker/shared/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
         http
             .cors(cors -> cors.configurationSource(request -> {
                 var config = new CorsConfiguration();
-                config.setAllowedOrigins(List.of("http://localhost:5173"));
+                config.setAllowedOrigins(List.of("http://localhost:5173", "https://weighttracker-two.vercel.app/"));
                 config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
                 config.setAllowedHeaders(List.of("*"));
                 return config;
