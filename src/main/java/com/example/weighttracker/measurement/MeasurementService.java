f1393df7ae46a0730503e5568109f49e1e1432e6
bumbens <bumbens@icloud.com>
2026-05-25 20:15:11 +0200

    Fix weight update on new measurement

--- src/main/java/com/example/weighttracker/measurement/MeasurementService.java
+++ src/main/java/com/example/weighttracker/measurement/MeasurementService.java
@@ -6,17 +6,28 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.weighttracker.user.UserServiceImpl;
+
 @Service
 public class MeasurementService {
     @Autowired
     private MeasurementRepository measurementRepository;
+
+    @Autowired
+    private UserServiceImpl userService;
 
     public List<Measurement> getMeasurementsByUserId(UUID userId) {
         return measurementRepository.findByUserId(userId);
     }
 
     public Measurement addMeasurement(Measurement measurement) {
-        return measurementRepository.save(measurement);
+
+        Measurement savedMeasurement = measurementRepository.save(measurement);
+
+        if(savedMeasurement.getMeasurementType().getName().equalsIgnoreCase("weight")){
+            userService.updateWeight(savedMeasurement.getUser().getId(), savedMeasurement.getValue());
+        }
+        return savedMeasurement;
     }
 
     public void deleteMeasurement(Long id) {
