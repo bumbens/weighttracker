e5632094c8d08277bcdfce2efcf55c2a7b227a83
bumbens <bumbens@icloud.com>
2026-05-25 20:19:04 +0200

    Fix fetch type for user and measurement type in Measurement entity, and update user weight when adding a new weight measurement.

--- src/main/java/com/example/weighttracker/measurement/Measurement.java
+++ src/main/java/com/example/weighttracker/measurement/Measurement.java
@@ -8,6 +8,7 @@ import com.example.weighttracker.user.User;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -28,11 +29,11 @@ public class Measurement {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne 
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @ManyToOne
+    @ManyToOne (fetch = FetchType.EAGER)
     @JoinColumn(name = "measurement_type_id", nullable = false)
     private MeasurementType measurementType;
 
